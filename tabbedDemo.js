2bd61707eb0addf8bdec6d7313a01c65ec7f96d2
Matt <contact@mattmcfarland.com>
2016-01-04 20:09:54 -0500

    upgrade docs

---
+++ tabbedDemo.js
@@ -0,0 +1,6 @@
+var React = require('react');
+var render = require('react-dom').render;
+var MarkedArea = require('../lib').MarkedArea
+
+
+render(<div><MarkedArea/></div>, document.getElementById('root'));
